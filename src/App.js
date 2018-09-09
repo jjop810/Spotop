47716bb575dea5426a955b236334b38c3c277a37
Jose Juan Otero Perez <josejsurfer@gmail.com>
2018-09-09 13:31:08 -0400

    Revert "added getting artist from the users playlist"
    This reverts commit ea9ca1fde6543c10cfbf3c940b66377fa716eebd.

--- src/App.js
+++ src/App.js
@@ -46,12 +46,12 @@ class App extends Component {
 
   componentDidMount() {
     let parsed = queryString.parse(window.location.search);
-    let accessToken = parsed.access_token;
-    if(!accessToken)
+    let accesToken = parsed.access_token;
+    if(!accesToken)
     return;
 
     fetch('https://api.spotify.com/v1/artists?ids=2R21vXR83lH98kGeO99Y66%2C6YIsL2oVmFhVL7EIKwKVQo%2C4q3ewBCX7sLwd24euuV69X%2C4SsVbpTthjScTS7U2hmr1X%2C4VMYDCV2IEDYJArk749S6m%2C5bWUlnPx9OYKsLiUJrhCA1', {
-      headers:{'Authorization': 'Bearer '+accessToken}
+      headers:{'Authorization': 'Bearer '+accesToken}
     }).then(response=>response.json())
     .then(data => this.setState({
       Artist: data.artists.sort(function(a, b){
@@ -68,50 +68,12 @@ class App extends Component {
       
       //this is a temp test function
       fetch('https://api.spotify.com/v1/artists?ids=2R21vXR83lH98kGeO99Y66%2C6YIsL2oVmFhVL7EIKwKVQo%2C4q3ewBCX7sLwd24euuV69X%2C4SsVbpTthjScTS7U2hmr1X', {
-        headers:{'Authorization': 'Bearer '+accessToken}
+        headers:{'Authorization': 'Bearer '+accesToken}
       }).then(response=>response.json())
       .then(data => console.log(data.artists[2]))
       
-
-      fetch('https://api.spotify.com/v1/me/playlists', {
-      headers: {'Authorization': 'Bearer ' + accessToken}
-    }).then(response => response.json())
-    .then(playlistData => {
-      let playlists = playlistData.items
-      let trackDataPromises = playlists.map(playlist => {
-        let responsePromise = fetch(playlist.tracks.href, {
-          headers: {'Authorization': 'Bearer ' + accessToken}
-        })
-        let trackDataPromise = responsePromise
-          .then(response => response.json())
-        return trackDataPromise
-      })
-      let allTracksDataPromises = 
-        Promise.all(trackDataPromises)
-      let playlistsPromise = allTracksDataPromises.then(trackDatas => {
-        trackDatas.forEach((trackData, i) => {
-          playlists[i].trackDatas = trackData.items
-            .map(item => item.track)
-            .map(trackData => ({
-              name: trackData.name,
-              artists: trackData.artists[0].id
-            }))
-        })
-        return playlists
-      })
-      return playlistsPromise
-    })
-    .then(playlists => this.setState({
-      playlists: playlists.map(item => {
-        return {
-          name: item.name,
-          imageUrl: item.images[0].url, 
-          songs: item.trackDatas
-        }
-    })
-    }))
-
   }
+
 
   render() {
     return (
